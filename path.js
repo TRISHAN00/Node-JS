82d968bac5d8138e41bdb4017a3021394a82da39
TRISHAN00 <trishansaha43@gmail.com>
2022-03-15 03:36:41 +0600

    create path using formate

--- path.js
+++ path.js
@@ -3,3 +3,12 @@ const myCurrentPath = __filename
 const dirName = __dirname
 
 console.log(path.basename(dirName));
+console.log(path.extname(myCurrentPath));
+
+let pathObj = {
+    dir: 'usr/local',
+    name: 'testFile',
+    ext: '.js'
+}
+
+console.log(path.format(pathObj));
